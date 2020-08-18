4c4358f075aef6a21d0182bd08e97286daf5688d
Tomas Pesek <pesekt@gmail.com>
2020-08-18 12:57:41 +0200

    -

--- src/test/java/swc3/server/JPAUnitTest.java
+++ src/test/java/swc3/server/JPAUnitTest.java
@@ -14,7 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-//@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
 public class JPAUnitTest {
 
   @Autowired
