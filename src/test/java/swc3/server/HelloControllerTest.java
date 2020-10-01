565bdecccedfb3bdbc8248127a87a81dd4676de0
pesekt1 <58634005+pesekt1@users.noreply.github.com>
2020-10-01 20:25:02 +0200

    -

---
+++ src/test/java/swc3/server/HelloControllerTest.java
@@ -0,0 +1,12 @@
+package swc3.server;
+
+import org.junit.jupiter.api.Test;
+import swc3.server.controller.HelloController;
+
+public class HelloControllerTest {
+    @Test
+    public void returnsHelloMessage(){
+        String message = new HelloController().hello("Tomas");
+        assert(message).equals("Hello Tomas!");
+    }
+}
