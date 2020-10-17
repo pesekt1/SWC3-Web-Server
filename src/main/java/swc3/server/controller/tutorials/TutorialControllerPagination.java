dd8d1f4dc145197ba66f2b8a45116153a4c51a55
pesekt1 <58634005+pesekt1@users.noreply.github.com>
2020-10-17 23:52:57 +0200

    -

--- src/main/java/swc3/server/controller/tutorials/TutorialControllerPagination.java
+++ src/main/java/swc3/server/controller/tutorials/TutorialControllerPagination.java
@@ -1,6 +1,5 @@
 package swc3.server.controller.tutorials;
 
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -8,23 +7,12 @@ import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 import org.springframework.data.domain.Sort.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import swc3.server.model.Tutorial;
 import swc3.server.repository.TutorialRepository;
 
