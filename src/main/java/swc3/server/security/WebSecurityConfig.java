649db7b7c73a52e8dd7bf5caeef0d3c90a344a86
pesekt1 <58634005+pesekt1@users.noreply.github.com>
2020-11-29 20:06:38 +0100

    spring security added

--- src/main/java/swc3/server/security/WebSecurityConfig.java
+++ src/main/java/swc3/server/security/WebSecurityConfig.java
@@ -37,7 +37,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-		authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+		authenticationManagerBuilder
+				.userDetailsService(userDetailsService)
+				.passwordEncoder(passwordEncoder());
 	}
 
 	@Bean
