60678b62b64cd5f7aba7cc5508939508e605073a
Tomas Pesek <pesekt@gmail.com>
2021-04-28 14:40:47 +0200

    -

--- src/test/java/swc3/server/IntegrationTests.java
+++ src/test/java/swc3/server/IntegrationTests.java
@@ -2,24 +2,36 @@ package swc3.server;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import swc3.server.config.Db1Config;
+import swc3.server.config.Db2Config;
+import swc3.server.config.Db3Config;
 import swc3.server.models.Tutorial;
 import swc3.server.repository.TutorialRepository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
+import javax.sql.DataSource;
+import javax.transaction.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 //@RunWith(SpringRunner.class)
 //@DataJpaTest
-@SpringBootTest(classes = {ServerApplication.class, Db1Config.class})
+//@AutoConfigureTestEntityManager
+@SpringBootTest(classes = {ServerApplication.class, Db1Config.class, Db2Config.class,Db3Config.class})
 @AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
 public class IntegrationTests {
 
-//  @Autowired
-//  private TestEntityManager entityManager;
+  //@Autowired
+  //private TestEntityManager entityManager;
 
   @Autowired
   TutorialRepository repository;
@@ -38,22 +50,25 @@ public class IntegrationTests {
   @Test
   public void should_store_a_tutorial() {
     Tutorial tutorial = repository.save(new Tutorial("Tut title", "Tut desc", true));
-
     assertThat(tutorial).hasFieldOrPropertyWithValue("title", "Tut title");
     assertThat(tutorial).hasFieldOrPropertyWithValue("description", "Tut desc");
     assertThat(tutorial).hasFieldOrPropertyWithValue("published", true);
   }
-//
+
 //  @Test
 //  public void should_find_all_tutorials() {
 //    Tutorial tut1 = new Tutorial("Tut#1", "Desc#1", true);
-//    entityManager.persist(tut1);
+//
+//    entityManagerFactory.createEntityManager().persist(tut1);
+//    //entityManager.persist(tut1);
 //
 //    Tutorial tut2 = new Tutorial("Tut#2", "Desc#2", false);
-//    entityManager.persist(tut2);
+//    entityManagerFactory.createEntityManager().persist(tut2);
+//    //entityManager.persist(tut2);
 //
 //    Tutorial tut3 = new Tutorial("Tut#3", "Desc#3", true);
-//    entityManager.persist(tut3);
+//    entityManagerFactory.createEntityManager().persist(tut3);
+//    //entityManager.persist(tut3);
 //
 //    Iterable<Tutorial> tutorials = repository.findAll();
 //
