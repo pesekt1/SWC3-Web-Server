850794c5bb523a249a3e78dc6bdf52da4238ba6b
Tomas Pesek <pesekt@gmail.com>
2021-04-29 18:52:45 +0200

    -

--- src/main/java/swc3/server/config/WebConfig.java
+++ src/main/java/swc3/server/config/WebConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
+//@EnableWebMvc //this did not work - static content from thymeleaf was not served...
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
