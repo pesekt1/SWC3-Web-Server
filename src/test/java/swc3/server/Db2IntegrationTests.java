dd8b01f9d95bb909315d8129f75f2a6da68e7b05
Tomas Pesek <pesekt@gmail.com>
2021-05-15 12:49:55 +0200

    integration tests

--- src/test/java/swc3/server/Db2IntegrationTests.java
+++ src/test/java/swc3/server/Db2IntegrationTests.java
@@ -1,6 +1,8 @@
 package swc3.server;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
@@ -10,7 +12,10 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 import swc3.server.Datasources.Db2.models.Tutorial_db2;
 import swc3.server.Datasources.Db2.repo.TutorialRepository_db2;
+import swc3.server.PrimaryDatasource.models.Tutorial;
 import swc3.server.config.Db2Config;
+
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,7 +23,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @AutoConfigureDataJpa
-@ContextConfiguration (classes = {Db2Config.class, TutorialRepository_db2.class})
+@ContextConfiguration (classes = {TutorialRepository_db2.class})
 @ComponentScan(resourcePattern = "**/Db2Config.class")
 @Transactional(transactionManager = "transactionManagerDb2")
 class Db2IntegrationTests {
@@ -33,8 +38,36 @@ class Db2IntegrationTests {
     }
 
     @Test
+    @DisplayName("Should not find any tutorials if repository is empty")
     void should_find_no_tutorials_if_repository_is_empty() {
         Iterable<Tutorial_db2> tutorials = repository.findAll();
         assertThat(tutorials).isEmpty();
+    }
+
+    @Test
+    @DisplayName("Should store a tutorial")
+    void should_store_a_tutorial() {
+        //we should not do it like this - to pass a tutorial object into save method because now the objects are connected:
+//        var newTutorial = new Tutorial_db2("Tut title", "Tut desc", true);
+//        var savedTutorial = repository.save(newTutorial); // save method returns the saved object
+//        Assertions.assertThat(savedTutorial).isEqualTo(newTutorial);
+
+        var tutorial = repository.save(new Tutorial_db2("Tut title", "Tut desc", true));
+        assertThat(tutorial)
+                .hasFieldOrPropertyWithValue("title", "Tut title")
+                .hasFieldOrPropertyWithValue("description", "Tut desc")
+                .hasFieldOrPropertyWithValue("published", true)
+                .hasFieldOrProperty("id").isNotNull();
+    }
+
+    @Test
+    void should_find_all_tutorials() {
+        //not a good way to test it - saveAll() is affecting the objects... it should be decoupled
+        var tut1 = new Tutorial_db2("Tut#1", "Desc#1", true);
+        var tut2 = new Tutorial_db2("Tut#2", "Desc#2", false);
+        var tut3 = new Tutorial_db2("Tut#3", "Desc#3", true);
+
+        repository.saveAll(Arrays.asList(tut1, tut2, tut3));
+        assertThat(repository.findAll()).hasSize(3).contains(tut1, tut2, tut3);
     }
 }
--- src/test/java/swc3/server/PrimaryDatasourceIntegrationTests.java
+++ src/test/java/swc3/server/PrimaryDatasourceIntegrationTests.java
@@ -14,6 +14,8 @@ import swc3.server.config.Db1Config;
 import swc3.server.PrimaryDatasource.models.Tutorial;
 import swc3.server.PrimaryDatasource.repository.TutorialRepository;
 
+import java.util.Arrays;
+
 // integration tests for persistence unit db1 - first data source,
 // transactionManager name comes from Db1Config class.
 // most of the tests are commented out because of the cloud database resource has low max connections limit
@@ -38,99 +40,99 @@ class PrimaryDatasourceIntegrationTests {
         Iterable<Tutorial> tutorials = repository.findAll();
         assertThat(tutorials).isEmpty();
     }
-//
-//    @Test
-//    void should_store_a_tutorial() {
-//        Tutorial tutorial = repository.save(new Tutorial("Tut title", "Tut desc", true));
-//        assertThat(tutorial)
-//            .hasFieldOrPropertyWithValue("title", "Tut title")
-//            .hasFieldOrPropertyWithValue("description", "Tut desc")
-//            .hasFieldOrPropertyWithValue("published", true);
-//    }
-//
-//    @Test
-//    void should_find_all_tutorials() {
-//        Tutorial tut1 = new Tutorial("Tut#1", "Desc#1", true);
-//        Tutorial tut2 = new Tutorial("Tut#2", "Desc#2", false);
-//        Tutorial tut3 = new Tutorial("Tut#3", "Desc#3", true);
-//
-//        repository.saveAll(Arrays.asList(tut1, tut2, tut3));
-//        assertThat(repository.findAll()).hasSize(3).contains(tut1, tut2, tut3);
-//    }
-//
-//    @Test
-//    void should_find_tutorial_by_id() {
-//        Tutorial tut1 = new Tutorial("Tut#1", "Desc#1", true);
-//        Tutorial tut2 = new Tutorial("Tut#2", "Desc#2", false);
-//        repository.saveAll(Arrays.asList(tut1, tut2));
-//
-//        Tutorial foundTutorial = repository.findById(tut2.getId()).get();
-//        assertThat(foundTutorial).isEqualTo(tut2);
-//    }
-//
-//    @Test
-//    void should_find_published_tutorials() {
-//        Tutorial tut1 = new Tutorial("Tut#1", "Desc#1", true);
-//        Tutorial tut2 = new Tutorial("Tut#2", "Desc#2", false);
-//        Tutorial tut3 = new Tutorial("Tut#3", "Desc#3", true);
-//        repository.saveAll(Arrays.asList(tut1, tut2, tut3));
-//
-//        Iterable<Tutorial> tutorials = repository.findByPublished(true);
-//        assertThat(tutorials).hasSize(2).contains(tut1, tut3);
-//    }
-//
-//    @Test
-//    void should_find_tutorials_by_title_containing_string() {
-//        Tutorial tut1 = new Tutorial("Spring Boot Tut#1", "Desc#1", true);
-//        Tutorial tut2 = new Tutorial("Java Tut#2", "Desc#2", false);
-//        Tutorial tut3 = new Tutorial("Spring Data JPA Tut#3", "Desc#3", true);
-//        repository.saveAll(Arrays.asList(tut1, tut2, tut3));
-//
-//        Iterable<Tutorial> tutorials = repository.findByTitleContaining("ring");
-//        assertThat(tutorials).hasSize(2).contains(tut1, tut3);
-//    }
-//
-//    @Test
-//    void should_update_tutorial_by_id() {
-//        Tutorial tut1 = new Tutorial("Tut#1", "Desc#1", true);
-//        Tutorial tut2 = new Tutorial("Tut#2", "Desc#2", false);
-//        repository.saveAll(Arrays.asList(tut1, tut2));
-//
-//        Tutorial updatedTut = new Tutorial("updated Tut#2", "updated Desc#2", true);
-//
-//        Tutorial tut = repository.findById(tut2.getId()).get();
-//        tut.setTitle(updatedTut.getTitle());
-//        tut.setDescription(updatedTut.getDescription());
-//        tut.setPublished(updatedTut.getPublished());
-//        repository.save(tut);
-//
-//        Tutorial checkTut = repository.findById(tut2.getId()).get();
-//
-//        assertThat(checkTut.getId()).isEqualTo(tut2.getId());
-//        assertThat(checkTut.getTitle()).isEqualTo(updatedTut.getTitle());
-//        assertThat(checkTut.getDescription()).isEqualTo(updatedTut.getDescription());
-//        assertThat(checkTut.getPublished()).isEqualTo(updatedTut.getPublished());
-//    }
-//
-//    @Test
-//    void should_delete_tutorial_by_id() {
-//        Tutorial tut1 = new Tutorial("Tut#1", "Desc#1", true);
-//        Tutorial tut2 = new Tutorial("Tut#2", "Desc#2", false);
-//        Tutorial tut3 = new Tutorial("Tut#3", "Desc#3", true);
-//        repository.saveAll(Arrays.asList(tut1, tut2,tut3));
-//
-//        repository.deleteById(tut2.getId());
-//
-//        Iterable<Tutorial> tutorials = repository.findAll();
-//        assertThat(tutorials).hasSize(2).contains(tut1, tut3);
-//    }
-//
-//    @Test
-//    void should_delete_all_tutorials() {
-//        repository.save(new Tutorial("Tut#1", "Desc#1", true));
-//        repository.save(new Tutorial("Tut#2", "Desc#2", false));
-//        repository.deleteAll();
-//
-//        assertThat(repository.findAll()).isEmpty();
-//    }
+
+    @Test
+    void should_store_a_tutorial() {
+        Tutorial tutorial = repository.save(new Tutorial("Tut title", "Tut desc", true));
+        assertThat(tutorial)
+            .hasFieldOrPropertyWithValue("title", "Tut title")
+            .hasFieldOrPropertyWithValue("description", "Tut desc")
+            .hasFieldOrPropertyWithValue("published", true);
+    }
+
+    @Test
+    void should_find_all_tutorials() {
+        Tutorial tut1 = new Tutorial("Tut#1", "Desc#1", true);
+        Tutorial tut2 = new Tutorial("Tut#2", "Desc#2", false);
+        Tutorial tut3 = new Tutorial("Tut#3", "Desc#3", true);
+
+        repository.saveAll(Arrays.asList(tut1, tut2, tut3));
+        assertThat(repository.findAll()).hasSize(3).contains(tut1, tut2, tut3);
+    }
+
+    @Test
+    void should_find_tutorial_by_id() {
+        Tutorial tut1 = new Tutorial("Tut#1", "Desc#1", true);
+        Tutorial tut2 = new Tutorial("Tut#2", "Desc#2", false);
+        repository.saveAll(Arrays.asList(tut1, tut2));
+
+        var foundTutorial = repository.findById(tut2.getId()).orElse(null); // if nothing is found, we set it to null
+        assertThat(foundTutorial).isEqualTo(tut2);
+    }
+
+    @Test
+    void should_find_published_tutorials() {
+        Tutorial tut1 = new Tutorial("Tut#1", "Desc#1", true);
+        Tutorial tut2 = new Tutorial("Tut#2", "Desc#2", false);
+        Tutorial tut3 = new Tutorial("Tut#3", "Desc#3", true);
+        repository.saveAll(Arrays.asList(tut1, tut2, tut3));
+
+        Iterable<Tutorial> tutorials = repository.findByPublished(true);
+        assertThat(tutorials).hasSize(2).contains(tut1, tut3);
+    }
+
+    @Test
+    void should_find_tutorials_by_title_containing_string() {
+        Tutorial tut1 = new Tutorial("Spring Boot Tut#1", "Desc#1", true);
+        Tutorial tut2 = new Tutorial("Java Tut#2", "Desc#2", false);
+        Tutorial tut3 = new Tutorial("Spring Data JPA Tut#3", "Desc#3", true);
+        repository.saveAll(Arrays.asList(tut1, tut2, tut3));
+
+        Iterable<Tutorial> tutorials = repository.findByTitleContaining("ring");
+        assertThat(tutorials).hasSize(2).contains(tut1, tut3);
+    }
+
+    @Test
+    void should_update_tutorial_by_id() {
+        Tutorial tut1 = new Tutorial("Tut#1", "Desc#1", true);
+        Tutorial tut2 = new Tutorial("Tut#2", "Desc#2", false);
+        repository.saveAll(Arrays.asList(tut1, tut2));
+
+        Tutorial updatedTut = new Tutorial("updated Tut#2", "updated Desc#2", true);
+
+        Tutorial tut = repository.findById(tut2.getId()).orElseThrow(); //throw exception if not found
+        tut.setTitle(updatedTut.getTitle());
+        tut.setDescription(updatedTut.getDescription());
+        tut.setPublished(updatedTut.getPublished());
+        repository.save(tut);
+
+        Tutorial checkTut = repository.findById(tut2.getId()).orElseThrow(); //throw exception if not found
+
+        assertThat(checkTut.getId()).isEqualTo(tut2.getId());
+        assertThat(checkTut.getTitle()).isEqualTo(updatedTut.getTitle());
+        assertThat(checkTut.getDescription()).isEqualTo(updatedTut.getDescription());
+        assertThat(checkTut.getPublished()).isEqualTo(updatedTut.getPublished());
+    }
+
+    @Test
+    void should_delete_tutorial_by_id() {
+        Tutorial tut1 = new Tutorial("Tut#1", "Desc#1", true);
+        Tutorial tut2 = new Tutorial("Tut#2", "Desc#2", false);
+        Tutorial tut3 = new Tutorial("Tut#3", "Desc#3", true);
+        repository.saveAll(Arrays.asList(tut1, tut2,tut3));
+
+        repository.deleteById(tut2.getId());
+
+        Iterable<Tutorial> tutorials = repository.findAll();
+        assertThat(tutorials).hasSize(2).contains(tut1, tut3);
+    }
+
+    @Test
+    void should_delete_all_tutorials() {
+        repository.save(new Tutorial("Tut#1", "Desc#1", true));
+        repository.save(new Tutorial("Tut#2", "Desc#2", false));
+        repository.deleteAll();
+
+        assertThat(repository.findAll()).isEmpty();
+    }
 }
