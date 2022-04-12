34c2556f375b2525d7ddb024fe52a5a6b3be6a0c
Tomas Pesek <pesekt@gmail.com>
2022-04-12 10:21:27 +0200

    comment

--- src/main/java/swc3/server/PrimaryDatasource/nativequeries/CustomersController2.java
+++ src/main/java/swc3/server/PrimaryDatasource/nativequeries/CustomersController2.java
@@ -17,6 +17,9 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+//connection string:
+//...?sessionVariables=transaction_isolation='READ COMMITTED'
+
 @RestController
 @RequestMapping("/extra")
 public class CustomersController2 {
