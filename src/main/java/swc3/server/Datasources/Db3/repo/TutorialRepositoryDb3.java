f60dd913a80f9558c4e7a43ae5bcc82cc3eabd32
Tomas Pesek <pesekt@gmail.com>
2023-09-01 02:53:22 +0200

    removed data sources

--- src/main/java/swc3/server/Datasources/Db3/repo/TutorialRepositoryDb3.java
+++
@@ -1,9 +0,0 @@
-package swc3.server.Datasources.Db3.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import swc3.server.Datasources.Db3.models.TutorialDb3;
-
-@RepositoryRestResource(path = "tutorialsDb3") //overriding the auto-generated api path
-public interface TutorialRepositoryDb3 extends JpaRepository<TutorialDb3, Integer> {
-}
--- src/main/java/swc3/server/Datasources/Db4/repo/TutorialRepositoryDb4.java
+++
@@ -1,9 +0,0 @@
-package swc3.server.Datasources.Db4.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import swc3.server.Datasources.Db4.models.TutorialDb4;
-
-@RepositoryRestResource(path = "tutorialsDb4") //overriding the auto-generated api path
-public interface TutorialRepositoryDb4 extends JpaRepository<TutorialDb4, Integer> {
-}
